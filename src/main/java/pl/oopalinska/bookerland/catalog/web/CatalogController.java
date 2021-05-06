ae9885ccb8690b3a2b06465a357c322685050635
OlgaOpalinska <olgaopalinska@wp.pl>
2021-05-06 17:55:28 +0200

    M4L15 - add Swagger

--- src/main/java/pl/oopalinska/bookerland/catalog/web/CatalogController.java
+++ src/main/java/pl/oopalinska/bookerland/catalog/web/CatalogController.java
@@ -3,6 +3,7 @@ package pl.oopalinska.bookerland.catalog.web;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -64,7 +65,7 @@ public class CatalogController {
         }
     }
 
-    @PutMapping("/{id}/cover")
+    @PutMapping(value = "/{id}/cover", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void addBookCover(@PathVariable Long id, @RequestParam("file") MultipartFile file) throws IOException {
         System.out.println("Got file: " + file.getOriginalFilename());
