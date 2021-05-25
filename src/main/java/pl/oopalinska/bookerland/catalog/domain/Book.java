561bec5a88d84b2d7e9fb2cd07bda9d5a79ac7bc
OlgaOpalinska <olgaopalinska@wp.pl>
2021-05-25 15:02:43 +0200

    M5L5 - enable JPA auditing

--- src/main/java/pl/oopalinska/bookerland/catalog/domain/Book.java
+++ src/main/java/pl/oopalinska/bookerland/catalog/domain/Book.java
@@ -4,18 +4,23 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @ToString
 @RequiredArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Book {
     @Id
     @GeneratedValue
@@ -25,6 +30,12 @@ public class Book {
     private Integer year;
     private BigDecimal price;
     private String coverId;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
     public Book(String title, String author, Integer year, BigDecimal price) {
         this.title = title;
--- src/main/java/pl/oopalinska/bookerland/order/domain/Order.java
+++ src/main/java/pl/oopalinska/bookerland/order/domain/Order.java
@@ -1,6 +1,9 @@
 package pl.oopalinska.bookerland.order.domain;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -12,6 +15,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "orders")
+@EntityListeners(AuditingEntityListener.class)
 public class Order {
 
     @Id
@@ -28,6 +32,10 @@ public class Order {
 
     private transient Recipient recipient;
 
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
 }
