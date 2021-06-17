31994f459ca0ed6fb050a7cb3b5825fc217c6501
OlgaOpalinska <olgaopalinska@wp.pl>
2021-06-17 14:47:23 +0200

    M7L3 - exception handler for IllegalArgumentException + fix the problem with displaying books few times

--- src/main/java/pl/oopalinska/bookerland/CustomGlobalExceptionHandler.java
+++ src/main/java/pl/oopalinska/bookerland/CustomGlobalExceptionHandler.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -19,17 +17,24 @@ public class CustomGlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Object> handleException(MethodArgumentNotValidException ex) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        body.put("timestamp", new Date());
-        body.put("status", status.value());
-
-        List<String> errors = ex
+        final List<String> errors = ex
                 .getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .map(x -> x.getField() + " - " + x.getDefaultMessage())
                 .collect(Collectors.toList());
+        return handle(HttpStatus.BAD_REQUEST, errors);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleException(IllegalArgumentException ex) {
+        return handle(HttpStatus.BAD_REQUEST, List.of(ex.getMessage()));
+    }
+
+    private ResponseEntity<Object> handle(HttpStatus status, List<String> errors) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
         body.put("errors", errors);
         return new ResponseEntity<>(body, status);
     }
