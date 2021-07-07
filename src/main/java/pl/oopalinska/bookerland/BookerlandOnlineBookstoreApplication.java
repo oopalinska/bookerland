042ee7cdb2ad461c50ff3f4c18a46dcfec97e099
OlgaOpalinska <olgaopalinska@wp.pl>
2021-07-07 15:03:46 +0200

    M8L9 - testing web layer (MockMVC)

--- src/main/java/pl/oopalinska/bookerland/BookerlandOnlineBookstoreApplication.java
+++ src/main/java/pl/oopalinska/bookerland/BookerlandOnlineBookstoreApplication.java
@@ -11,7 +11,6 @@ import org.springframework.web.client.RestTemplate;
 import pl.oopalinska.bookerland.order.application.OrdersProperties;
 
 @EnableScheduling
-@EnableJpaAuditing
 @SpringBootApplication
 @EnableConfigurationProperties(OrdersProperties.class)
 public class BookerlandOnlineBookstoreApplication {
---
+++ src/test/java/pl/oopalinska/bookerland/catalog/web/CatalogControllerWebTest.java
@@ -0,0 +1,42 @@
+package pl.oopalinska.bookerland.catalog.web;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import pl.oopalinska.bookerland.catalog.application.port.CatalogUseCase;
+import pl.oopalinska.bookerland.catalog.domain.Book;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest({CatalogController.class})
+class CatalogControllerWebTest {
+
+    @MockBean
+    CatalogUseCase catalogUseCase;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void shouldGetAllBooks() throws Exception {
+        //given
+        Book effectiveJava = new Book("Effective Java", 2005, new BigDecimal("99.00"), 50L);
+        Book concurrency = new Book("Java Concurrency in Practice", 2006, new BigDecimal("99.00"), 50L);
+        Mockito.when(catalogUseCase.findAll()).thenReturn(List.of(effectiveJava, concurrency));
+        //expect
+        mockMvc.perform(get("/catalog"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+}
