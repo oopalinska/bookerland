484e2174c46efb9bfd87c87a94c685ec461b1b67
OlgaOpalinska <olgaopalinska@wp.pl>
2021-07-07 12:51:05 +0200

    M8L6 - testing only JPA layer - @DataJpaTest

---
+++ src/test/java/pl/oopalinska/bookerland/order/application/ManipulateOrderServiceTest.java
@@ -0,0 +1,51 @@
+package pl.oopalinska.bookerland.order.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import pl.oopalinska.bookerland.catalog.db.BookJpaRepository;
+import pl.oopalinska.bookerland.catalog.domain.Book;
+import pl.oopalinska.bookerland.order.domain.Recipient;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static pl.oopalinska.bookerland.order.application.port.ManipulateOrderUseCase.*;
+
+@DataJpaTest
+@Import(ManipulateOrderService.class)
+class ManipulateOrderServiceTest {
+
+    @Autowired
+    BookJpaRepository bookRepository;
+    @Autowired
+    ManipulateOrderService service;
+
+    @Test
+    public void userCanPlaceOrder() {
+        //given
+        Book effectiveJava = givenEffectiveJava(50L);
+        Book jcip = givenJavaConcurrency(50L);
+        PlaceOrderCommand command = PlaceOrderCommand
+                .builder()
+                .recipient(recipient())
+                .item(new OrderItemCommand(effectiveJava.getId(), 10))
+                .item(new OrderItemCommand(jcip.getId(), 10))
+                .build();
+        //when
+        PlaceOrderResponse response = service.placeOrder(command);
+        //then
+        assertTrue(response.isSuccess());
+
+    }
+    private Book givenJavaConcurrency(long available) {
+        return bookRepository.save(new Book("Java Concurrency in Practice", 2006, new BigDecimal("99.90"), available));
+    }
+    private Book givenEffectiveJava(long available) {
+        return bookRepository.save(new Book("Effective Java", 2005, new BigDecimal("199.90"), available));
+    }
+    private Recipient recipient() {
+        return Recipient.builder().email("john@example.org").build();
+    }
+}
