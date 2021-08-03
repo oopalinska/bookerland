95f0fb17547ba4ddf0d3003037f746ee8bd1bccd
OlgaOpalinska <olgaopalinska@wp.pl>
2021-08-03 11:00:23 +0200

    M10L5 - secure Swagger

--- src/main/java/pl/oopalinska/bookerland/security/BookerlandSecurityConfiguration.java
+++ src/main/java/pl/oopalinska/bookerland/security/BookerlandSecurityConfiguration.java
@@ -41,6 +41,7 @@ public class BookerlandSecurityConfiguration extends WebSecurityConfigurerAdapte
                 .authorizeRequests()
                 .mvcMatchers(HttpMethod.GET, "/catalog/**", "/uploads/**", "/authors/**").permitAll()
                 .mvcMatchers(HttpMethod.POST, "/orders", "/login", "/users").permitAll()
+                .mvcMatchers("/swagger-ui/**", "/v3/api-docs/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
              .and()
                 .httpBasic()
